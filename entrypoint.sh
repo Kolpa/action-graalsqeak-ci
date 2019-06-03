ea9957230c9f7b9a4ee6a7cfb8805c8f680926c8
Kolya Opahle <kolyaopahle@gmail.com>
2019-06-03 13:07:43 +0200

    added classpath fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 Xvfb -screen 0 800x600x16 -ac &
+
 cd $GITHUB_WORKSPACE
+
+DIR="$( pwd )"
+sed -i "s#addToClassPath: '.*'#addToClassPath: \'$DIR/Java-Toolbuilder/src\'#g" $DIR"/squeak-smalltalk-packages/Toolbuilder-Java.package/JavaToolBuilder.class/class/addJavaClasses.st"
+
 /smalltalkCI/run.sh
